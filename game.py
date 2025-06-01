9928b55add48528dc9856175abeeec8911be939c
BEBRIK18 <vinniamink@gmail.com>
2025-06-01 13:44:41 +0300

    I created a new mechanics for scala

--- game.py
+++ game.py
@@ -26,10 +26,24 @@ class Player(GameSprite):
 class Shrak(GameSprite):
     def update(self):
         self.rect.x -= self.speed
+
+    def throwing_shrek(self):
         if self.rect.x <= 0:
             self.rect.x = 650
             self.rect.y = randint(50, 450)
             self.speed = randint(2, 5)
+    def throwing_shrek_from_scala(self):
+        self.rect.x = 650
+        self.rect.y = randint(50, 450)
+        self.speed = randint(2, 5)
+
+class Shield(GameSprite):
+    def update(self):
+        self.rect.x -= self.speed
+        if self.rect.x <= 0:
+            self.rect.x = 1500
+            self.rect.y = randint(30, 470)
+            self.speed = randint(2, 4)
 
 
 
@@ -47,6 +61,8 @@ shrak2 = Shrak('shrak.png', 650, 170, 2, 75, 75)
 shrak3 = Shrak('shrak.png', 650, 250, 3, 70, 70)
 shrak4 = Shrak('shrak.png', 650, 360, 4, 65, 65)
 shrak5 = Shrak('shrak.png', 650, 450, 5, 60, 60)
+
+shield = Shield('shield.png', 700, 300, 2, 50, 50)
 
 shraks.add(shrak1)
 shraks.add(shrak2)
@@ -66,6 +82,7 @@ FPS = 60
 clock = time.Clock()
 game = True
 finish = False
+protection = False
 
 while game:
     key_pressed = key.get_pressed()
@@ -75,11 +92,30 @@ while game:
         player.update()
         shraks.draw(window)
         shraks.update()
+        for sprite_object in shraks:
+            sprite_object.throwing_shrek()
+        shield.update()
 
 
-    grc = sprites_list = sprite.spritecollide(player, shraks, False)
+    grc = sprite.spritecollide(player, shraks, False)
+    grc2 = sprite.collide_rect(player, shield)
 
-    if grc :
+    if grc2 :
+        protection = True
+        
+    if grc and protection == True:
+        for sprite_object in shraks:
+            grc3 = sprite.collide_rect(player, sprite_object)
+            if grc3:
+                print(1)
+                print(protection)
+                sprite_object.throwing_shrek_from_scala()
+                grc = False
+        protection = False
+        
+
+    if grc and protection != True:
+        print(protection)
         finish = True
         lose = font.render('шрек тебя съел', True, (255, 0, 0))
         window.blit(lose, (300, 200))
@@ -91,6 +127,7 @@ while game:
             game = False
 
     player.reset()
+    shield.reset()
     clock.tick(FPS)
 
     display.update()
